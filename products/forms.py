f6519a33ceb933e33e3a5cc2c83c9c68603aa37f
Olaf Tomaszewski <tomaszewskiolaf@gmail.com>
2021-06-13 18:23:13 +0200

    3 different emgs

--- products/forms.py
+++ products/forms.py
@@ -12,17 +12,47 @@ class ProductForm(forms.ModelForm):
         self.fields['description'].widget.attrs = {
             'class': 'form-control col-md-6'
         }
-        self.fields['average_emg'].widget.attrs = {
+        self.fields['average_emg_proper_muscle'].widget.attrs = {
             'class': 'form-control col-md-6',
             'step': 'any',
             'min': '1',
         }
-        self.fields['max_emg'].widget.attrs = {
+        self.fields['average_emg_antagonist_muscle'].widget.attrs = {
             'class': 'form-control col-md-6',
             'step': 'any',
             'min': '1',
         }
-        self.fields['min_emg'].widget.attrs = {
+        self.fields['average_emg_wrong_muscle'].widget.attrs = {
+            'class': 'form-control col-md-6',
+            'step': 'any',
+            'min': '1',
+        }
+        self.fields['max_emg_proper_muscle'].widget.attrs = {
+            'class': 'form-control col-md-6',
+            'step': 'any',
+            'min': '1',
+        }
+        self.fields['max_emg_antagonist_muscle'].widget.attrs = {
+            'class': 'form-control col-md-6',
+            'step': 'any',
+            'min': '1',
+        }
+        self.fields['max_emg_wrong_muscle'].widget.attrs = {
+            'class': 'form-control col-md-6',
+            'step': 'any',
+            'min': '1',
+        }
+        self.fields['min_emg_proper_muscle'].widget.attrs = {
+            'class': 'form-control col-md-6',
+            'step': 'any',
+            'min': '1',
+        }
+        self.fields['min_emg_antagonist_muscle'].widget.attrs = {
+            'class': 'form-control col-md-6',
+            'step': 'any',
+            'min': '1',
+        }
+        self.fields['min_emg_wrong_muscle'].widget.attrs = {
             'class': 'form-control col-md-6',
             'step': 'any',
             'min': '1',
@@ -30,4 +60,4 @@ class ProductForm(forms.ModelForm):
 
     class Meta:
         model = Product
-        fields = ('name', 'description', 'average_emg', 'max_emg', 'min_emg')
+        fields = ('name', 'description', 'average_emg_proper_muscle', 'average_emg_antagonist_muscle', 'average_emg_wrong_muscle', 'max_emg_proper_muscle', 'max_emg_antagonist_muscle', 'max_emg_wrong_muscle', 'min_emg_proper_muscle', 'min_emg_antagonist_muscle', 'min_emg_wrong_muscle')
