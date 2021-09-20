b7d96da7cdbe953047771d827b571147d0e80641
Olaf Tomaszewski <tomaszewskiolaf@gmail.com>
2021-09-20 21:55:48 +0200

    htmls

--- conceptu/templatetags/filter.py
+++ conceptu/templatetags/filter.py
@@ -16,7 +16,7 @@ def split_by_newline(value):
 
 @register.filter(name='convert_list_to_int')
 def convert_list_to_int(list_of_values):
-    return [int(float(element)) for element in list_of_values]
+    return [int(float(element)) for element in list_of_values if element.replace('.','',1).isdigit()]
 
 
 @register.filter(name='get_time_vector')
@@ -28,3 +28,13 @@ def get_time_vector(list_of_values):
 def convert_to_charts(list_of_values):
     time_vector = get_time_vector(list_of_values)
     return list([time, value] for time, value in zip(time_vector, list_of_values))
+
+
+@register.filter(name='get_max_value')
+def get_max_value(signal):
+    return max(signal)
+
+
+@register.filter(name='get_min_value')
+def get_min_value(signal):
+    return min(signal)
